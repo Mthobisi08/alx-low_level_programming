6c7ae22b40a13d99c8afbbdb5bbf2ab49f17abb3
Mthobisi <mthobisiks90@gmail.com>
2023-02-17 16:30:07 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%i", a);
+	printf("\n");
+	return (0);
+}
