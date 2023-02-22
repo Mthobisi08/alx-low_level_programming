ea9b0fa367ccdc17fac20a681112eb4e88346eae
Mthobisi <mthobisiks90@gmail.com>
2023-02-22 12:55:14 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests function that print if integer is positiveor nagative
- * Return: 0
- */
+* main - tests function that prints if integer
+* Return: 0
+*/
+
 int main(void)
 {
-	int i;
+int i;
 
-	1 = 0;
-	positive_or_nagative(1);
-	Return(0);
+i = 98;
+positive_or_negative(i);
+
+return (0);
 }
