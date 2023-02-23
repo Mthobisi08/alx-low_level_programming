beb5e896debf14f508038e3d10344486d4061d88
Mthobisi <mthobisiks90@gmail.com>
2023-02-23 14:13:43 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,22 @@
 #include "main.h"
-
 /**
- * more_numbers - entry point
- *
- * Description: Prints the numbers with _putchar
+ * print_diagonal - printing lines diagonal
+ * @n: input variable
  *
  * Return: void
  */
-
-void more_numbers(void)
+void print_diagonal(int n)
 {
-	int number, tens, units, row;
-
-	for (row = 1; row <= 10; row++)
-	{
-		for (number = 0; number <= 14; number++)
-		{
-			tens = number / 10;
-			units = number % 10;
-			if (number > 9)
-				_putchar(tens + '0');
-
-			_putchar(units + '0');
-		}
-		_putchar('\n');
-	}
+int i, j;
+if (n <= 0)
+putchar('\n');
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+putchar(' ');
+}
+putchar('\\');
+putchar('\n');
+}
 }
