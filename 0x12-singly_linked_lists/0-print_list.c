1e0d6844669119619a4fde77013669d407da9f76
Mthobisi <mthobisiks90@gmail.com>
2023-03-30 14:24:22 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,44 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
+#include <string.h>
+#include <stdio.h>
 
 /**
- * print_list - print the elements of a linked list.
- * @h: liked list head.
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Return: amount of nodes of the linked list.
- *
+ * Description: singly linked list node structure
+ * for Holberton project
  */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: head of linked list
+ *
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+    size_t count = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		count++;
-	}
-	return (count);
+    while (h != NULL)
+    {
+        if (h->str != NULL)
+            printf("[%d] %s\n", h->len, h->str);
+        else
+            printf("[0] (nil)\n");
+        count++;
+        h = h->next;
+    }
+
+    return (count);
 }
+
+
